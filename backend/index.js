c4fb53a4b4ac34472eadebc361c040eb3f5e208a
mohdmusab701 <mohdmusab701@gmail.com>
2026-06-28 04:23:48 +0530

    Update project

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+// Set CORS for frontend URL / allow single-node deploy
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,6 +25,19 @@ app.use('/api/products', require('./routes/productRoutes.js'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payment', require('./routes/paymentRoutes.js'));
 app.use('/api/analytics', require('./routes/analyticsRoutes.js'));
+
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('ShopNest API is running in Development mode...');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
